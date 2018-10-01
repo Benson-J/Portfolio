b7c14a5e8f5b76f4888b2808310b6d8fdc451796
James <j.benson13@hotmail.co.uk>
2018-10-01 11:34:26 +0100

    Started building dashboard

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type ="text/css" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>James | Admin</title>
+</head>
+<body>
+    <input type="checkbox" name="homeCheck">
+    Home
+    <form method="post" action="#">
+        <input type="text" name="homeTitle" placeholder="Name"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
