c981c6829cf78937e8b29efae1841f0e223ee68c
James <j.benson13@hotmail.co.uk>
2018-10-01 13:25:53 +0100

    Created individual pages to edit each main page

--- admin.php
+++ admin.php
@@ -4,16 +4,14 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="normalize.css">
     <link rel="stylesheet" type ="text/css" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    <link rel="stylesheet" type="text/css" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>James | Admin</title>
 </head>
 <body>
-    <input type="checkbox" name="homeCheck">
-    Home
-    <form method="post" action="#">
-        <input type="text" name="homeTitle" placeholder="Name"><br>
-        <input type="submit">
-    </form>
+    <a href="editHome.php">Home</a>
+    <a href="editAbout.php">About</a>
+    <a href="editPortfolio.php">Portfolio</a>
+    <a href="editContact.php">Contact</a>
+    <a href="editSocial.php">Social</a>
 </body>
 </html>
