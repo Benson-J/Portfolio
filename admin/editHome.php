ca062b7d6ef31c754b506b429768134fd7f55742
James <j.benson13@hotmail.co.uk>
2018-10-01 14:20:35 +0100

    Started adding edit page specific forms

--- admin/editHome.php
+++ admin/editHome.php
@@ -2,12 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="normalize.css">
-    <link rel="stylesheet" type ="text/css" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../normalize.css">
     <title>James | Admin/Home</title>
 </head>
 <body>
-    <a href="admin.php">Back</a>
+<h3>Home title text</h3>
+    <form method="post" action="db.php">
+        <textarea name="homeTitle"></textarea><br>
+        <input type="submit">
+    </form>
+    <a href=".">Back</a>
 </body>
 </html>
