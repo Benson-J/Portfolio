536b6e8da2ee358c6694b9a732e14e0587723c2e
James <j.benson13@hotmail.co.uk>
2018-10-02 17:02:22 +0100

    Began optimising functions for testing

--- admin/dbDisplay.php
+++ admin/dbDisplay.php
@@ -4,34 +4,37 @@ $db = new PDO('mysql:dbname=portfolio;host=127.0.0.1', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $stmtAbout = $db->query('SELECT `about`.`id`,`about`.`text` AS `infoA`,`aboutTypes`.`column` AS `infoB` FROM `about` JOIN `aboutTypes` ON `about`.`column_id` = `aboutTypes`.`id`;');
+$dataAbout = $stmtAbout->fetchAll();
 $stmtContact = $db->query('SELECT `id`,`text` AS `infoA`,`link` AS `infoB` FROM `contact`;');
+$dataContact = $stmtContact->fetchAll();
 $stmtSocial = $db->query('SELECT `id`,`icon` AS `infoA`,`link` AS `infoB` FROM `socialLinks`;');
+$dataSocial = $stmtSocial->fetchAll();
 $stmtPortfolio = $db->query('SELECT `id`,`title`,`link`,`github`,`image`,`description` FROM `portfolio`;');
+$dataPortfolio = $stmtPortfolio->fetchAll();
 
 /*
  * Displays heading text of a given page
  *
- * @param $db is the database containing the heading text
+ * @param PDO $db is the database containing the heading text
  * @param string $pageName is the name of the page whose heading will be displayed
  *
  * @return string is the heading text of the chosen page
  */
-function displayTitle($db, string $pageName) : string {
+function displayTitle(PDO $db, string $pageName) : string {
     $stmt = $db->query('SELECT `content` FROM `titles` WHERE `page` = "' . $pageName . '";');
     $data = $stmt->fetch();
-    return $data['content'];
+    return $data;
 }
 
 /*
  * Echoes 2 form inputs & a checkbox per item, for editing or deleting
  *
- * @param $stmt is the object containing the default info for the inputs
+ * @param $data is the array containing the data
  * @param $pageName is the name if the page that needs the data
  *
  * @return string is the html for the form text inputs & delete checkboxes
  */
-function displayEntries($stmt, string $pageName) : string {
-    $data = $stmt->fetchAll();
+function displayEntries($data, string $pageName) : string {
     $echoedString = '';
     foreach ($data as $row) {
         $echoedString .= '
@@ -45,12 +48,11 @@ function displayEntries($stmt, string $pageName) : string {
 /*
  * Echoes a set of forms containing items on portfolio page, for editing or deleting them
  *
- * @param $stmt is the statement containing all the portfolio items
+ * @param $data is the array containing all the portfolio items
  *
  * @return string is the html for the form text inputs & delete checkboxes
  */
-function displayPortfolio($stmt) : string {
-    $data = $stmt->fetchAll();
+function displayPortfolio($data) : string {
     $echoedString = '';
     foreach ($data as $row) {
         $echoedString .= '
