50ea39a320c4cea382709d3eea9d761cb7bd6c07
James <j.benson13@hotmail.co.uk>
2018-10-02 15:44:28 +0100

    Added missing curly bracket

--- test/admin/dbDisplay.php
+++ test/admin/dbDisplay.php
@@ -5,10 +5,12 @@ require('../../admin/dbDisplay.php');
 use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase {
-    public function testdisplayTitle_success() {
+    public function testdisplayTitle_success()
+    {
         $db = new PDO('mysql:dbname=portfolio;host=127.0.0.1', 'root');
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
         $output = displayTitle($db, 'about');
         $this->assertInternalType('string', $output);
+    }
 }
